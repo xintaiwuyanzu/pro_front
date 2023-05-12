511fb4188a5a6fd9c996451f13203807eac9e1bb
jinchen <986749726@qq.com>
2023-05-12 17:52:14 +0800

    字典管理添加编辑样式

--- framework/src/views/system/dict/index.vue
+++ framework/src/views/system/dict/index.vue
@@ -1,3 +1,11 @@
+<template>
+    <table-index path="sysDict"
+                 :fields=fields
+                 :defaultInsertForm=defaultInsertForm>
+    </table-index>
+</template>
+
+<script>
 /**
  * 字典模块
  */
@@ -9,7 +17,7 @@ export default {
                 {prop: 'key', label: '字典编码', align: 'center', search: true, required: true},
                 {prop: 'value', label: '字典值', required: true},
                 {prop: 'description', label: '描述', width: "300", type: 'textarea'},
-                {prop: 'order', label: '排序', width: "80"},
+                {prop: 'order', label: '排序', width: "80", type:'number'},
                 {
                     prop: 'status',
                     label: '是否可用',
@@ -22,10 +30,18 @@ export default {
                     fieldType: 'select',
                     component: 'tag'
                 }
-            ]
+            ],
+            defaultInsertForm: {status: '1'}
         }
     },
-    render() {
-        return <table-index path="sysDict" fields={this.fields}/>
-    }
+    // render() {
+    //     return <table-index path="sysDict" fields={this.fields} defaultInsertForm={this.defaultInsertForm}/>
+    // }
 }
+</script>
+
+<style scoped>
+/deep/.el-select {
+    display: block;
+}
+</style>
